55bfe21274358ddb568d36a149253a881802da38
elad.k.int <elad.k.int@indiesemi.com>
2026-04-02 12:21:09 +0300

    Did the first part

---
+++ decode.c
@@ -0,0 +1,12 @@
+long decode_c_version(long x, long y, long z)
+{
+    y = y-z;
+    x = y*x;
+    
+    long ret = 0;
+    ret = y;
+    ret = ret<<63;
+    ret = ret>>63;
+    ret = ret^x;
+    return ret;
+}
